9baedfdb9ac2c67f0633d7efd743b7a26a756dd5
Joelton Luz <joelton@live.com>
2021-10-07 20:21:52 -0300

    initial commit

---
+++ src/App.styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #24282f;
+  min-height: 100vh;
+  color: #fff;
+`;
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as C from './App.styles';
+import './App.css';
+
+function App() {
+  return (
+    <C.Container>
+      ...
+    </C.Container>
+  );
+}
+
+export default App;
